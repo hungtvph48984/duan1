0f36d69509d1547201752109545a6ab6c101f74e
quangchi04 <YOUR_EMAIL>
2025-04-16 11:16:45 +0700

    update login

--- mvc-oop-basic/views/auth/formDangKy.php
+++ mvc-oop-basic/views/auth/formDangKy.php
@@ -24,7 +24,8 @@ require_once 'views/layout/menu.php'; ?>
 
     <!-- login register wrapper start -->
     <div class="login-register-wrapper section-padding">
-        <div class="container" style="max-width:40vw">
+        <div class="container" style="max-width:40vw"> 
+            
             <div class="member-area-from-wrap">
                 <div class="row">
                     <!-- Register Content Start -->
